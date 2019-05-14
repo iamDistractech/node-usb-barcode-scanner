7e43e1cb38b9458fe5e4effaf749e6137097055d
Thijs Spijker <thijs@tsnetwork.nl>
2019-05-14 15:36:37 +0200

    first release

---
+++ index.js
@@ -0,0 +1,45 @@
+const HID = require('node-hid').HID;
+const EventEmitter = require('events');
+const hidMap = require('./hidmap');
+
+class UsbScanner extends EventEmitter {
+
+	constructor(options) {
+		let { vendorID, productID, path } = options;
+
+		super();
+
+		if(path) this.hid = new HID(path);
+		else if (vendorID && productID) this.hid = new HID(vendorID, productID);
+		else console.error('Device cannot be found, please supply a path or VID & PID'); // eslint-disable-line
+
+		this._hidMap = hidMap.standard;
+		this._hidMapShift = hidMap.shift;
+
+		// Bind 'this' to the methods
+		this.startScanning = this.startScanning.bind(this);
+	}
+
+	startScanning() {
+		let barcode = [];
+
+		this.hid.on('data', (data) => {
+			const modifierValue = data[0];
+			const characterValue = data[2];
+
+			if (characterValue !== 0) {
+				if (modifierValue === 2 || modifierValue === 20) {
+					barcode.push(this._hidMapShift[characterValue]);
+				} else if (characterValue !== 40) {
+					barcode.push(this._hidMap[characterValue]);
+				} else if (characterValue === 40) {
+					let scanResult = barcode.join('');
+					barcode = [];
+					this.emit('data', scanResult);
+				}
+			}
+		});
+	}
+}
+
+module.exports = UsbScanner;

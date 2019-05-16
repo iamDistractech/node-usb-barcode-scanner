8d342807b51fcb67181a619567393794b54b9789
Thijs <thijs@tsnetwork.nl>
2019-05-16 15:05:25 +0200

    v1.1.0 Release
    This release adds some new functionality
    
    - A method that returns devices on the computer (so you can see the VID/PID)
    - vCard support:
      - A scanned vCard now returns one string containing all data
      - this behaviour is default on but can be turned off by passing `vCardString: false` into the options object
      - The default separation is a pipe character `'|'`, but can be changed by passing `vCardSeparator: <string>` into the options object
    - UTF8 Character coding is removed
    - Added a test environment

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const HID = require('node-hid').HID;
+const devices = require('node-hid').devices();
 const EventEmitter = require('events');
 const hidMap = require('./hidmap');
 
 class UsbScanner extends EventEmitter {
 
 	constructor(options) {
-		let { vendorID, productID, path } = options;
-
+		let { 
+			vendorID,
+			productID,
+			path,
+			vCardString = true,
+			vCardSeperator = '|'
+		} = options;
+		
 		super();
+
+		this._vCardString = vCardString;
+		this._vCardSeperator = vCardSeperator;
 
 		if(path) this.hid = new HID(path);
 		else if (vendorID && productID) this.hid = new HID(vendorID, productID);
@@ -20,8 +30,14 @@ class UsbScanner extends EventEmitter {
 		this.startScanning = this.startScanning.bind(this);
 	}
 
+	static showDevices() {
+		return devices;
+	}
+
+
 	startScanning() {
-		let barcode = [];
+		let scanResult = [];
+		let vCard = [];
 
 		this.hid.on('data', (data) => {
 			const modifierValue = data[0];
@@ -29,17 +45,42 @@ class UsbScanner extends EventEmitter {
 
 			if (characterValue !== 0) {
 				if (modifierValue === 2 || modifierValue === 20) {
-					barcode.push(this._hidMapShift[characterValue]);
+					scanResult.push(this._hidMapShift[characterValue]);
 				} else if (characterValue !== 40) {
-					barcode.push(this._hidMap[characterValue]);
+					scanResult.push(this._hidMap[characterValue]);
 				} else if (characterValue === 40) {
-					let scanResult = barcode.join('');
-					barcode = [];
-					this.emit('data', scanResult);
+					let barcode = scanResult.join('');
+					scanResult = [];
+
+					barcode = removeUTF8(barcode);
+					
+					if (this._vCardString) {
+						if (barcode === 'BEGIN:VCARD') {
+							vCard.push(barcode);
+						} else if (barcode === 'END:VCARD') {
+							vCard.push(barcode);
+							vCard = vCard.join(this._vCardSeperator);
+							this.emit('data', vCard);
+							vCard = [];
+						} else if (vCard.length > 0 ) {
+							vCard.push(barcode);
+						} else this.emit('data', barcode);
+					} else {
+						this.emit('data', barcode);
+					}
 				}
 			}
 		});
 	}
+}
+
+
+function removeUTF8(barcode) {
+	let utf8 = barcode.slice(0, 7);
+	if (utf8 === '\\000026') {
+		barcode = barcode.slice(7);
+		return barcode;
+	} else return barcode;
 }
 
 module.exports = UsbScanner;
---
+++ test-utils/test.js
@@ -0,0 +1,17 @@
+const UsbScanner = require('../index');
+require('dotenv').config();
+
+const options = {
+	vendorID: process.env.VENDOR_ID,
+	productID: process.env.PRODUCT_ID,
+	path: undefined,
+};
+
+const scanner = new UsbScanner(options);
+
+scanner.on('data', (data) => console.log(data)); //eslint-disable-line
+
+// let devices = UsbScanner.showDevices();
+// console.log(devices); 
+
+scanner.startScanning();

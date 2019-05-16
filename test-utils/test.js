cf6f2421dcad7b98c9cf6569a1f0214e062749f3
Thijs Spijker <thijs@tsnetwork.nl>
2019-05-16 11:39:40 +0200

    Add test environment

---
+++ test-utils/test.js
@@ -0,0 +1,14 @@
+const UsbScanner = require('../index');
+require('dotenv').config();
+
+const options = {
+	vendorID: process.env.VENDOR_ID,
+	productID: process.env.PRODUCT_ID,
+	path: undefined
+};
+
+const scanner = new UsbScanner(options);
+
+scanner.on('data', (data) => console.log(data)); //eslint-disable-line
+
+scanner.startScanning();
